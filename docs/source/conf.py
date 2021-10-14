fcefc0108c9fc0618b8122f2501b9d4f9d05f311
Malte Titze <mtitze@users.noreply.github.com>
2021-10-14 12:48:31 +0200

    new version 0.1.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Malte Titze'
 author = 'Malte Titze'
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.2"
+release = "0.1.3"
 
 
 # -- General configuration ---------------------------------------------------
