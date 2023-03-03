a8fcb15b344f449954751ca0fbefa70666734821
Malte Titze <mtitze@users.noreply.github.com>
2023-03-03 22:37:53 +0100

    more docs dependencies

--- setup.py
+++ setup.py
@@ -33,7 +33,8 @@ setup(
     package_dir={"": "."},
     package_data={},
     install_requires=[
-        'numpy==1.*,>=1.21.2', 'sympy==1.*,>=1.8.0', 'more_itertools==9.*,>=9.0.0'
+        'numpy==1.*,>=1.21.2', 'sympy==1.*,>=1.8.0',
+        'more_itertools==9.*,>=9.0.0', 'tqdm==4.*,>=4.64.0'
     ],
     extras_require={"dev": ["ipykernel==6.*,>=6.4.1"]},
 )
