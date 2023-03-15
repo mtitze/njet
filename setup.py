6005442cc927d89ca8b7baadb4f6db65de5e3f3b
Malte Titze <mtitze@users.noreply.github.com>
2023-03-15 09:09:30 +0100

    version update

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_long_description():
 setup(
     long_description=get_long_description(),
     name='njet',
-    version='0.4.7',
+    version='0.4.8',
     description='Lightweight automatic differentiation package for higher-order differentiation.',
     long_description_content_type='text/markdown',
     python_requires='<3.11,>=3.8',
